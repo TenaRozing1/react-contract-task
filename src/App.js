ba0c6ec755d31952e2e0343fc2c271f0f6e276cd
Tena Rozing <tena.rozing@gauss.dev>
2024-02-26 21:17:55 +0100

    Imported components in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ContractForm from "./components/ContractForm";
+import ContractFilter from "./components/ContractFilter";
+import ContractTable from "./components/ContractTable";
 
-function App() {
+const App = () => {
+  const contracts = [
+    {
+      contractName: "Contract 1",
+      client: "John Doe",
+      startDate: "2023-01-01",
+      duration: 12,
+      comments: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      contractName: "Contract 2",
+      client: "Anne Hathaway",
+      startDate: "2023-02-15",
+      duration: 6,
+      comments: "Alice in wonderland.",
+    },
+    {
+      contractName: "Contract 3",
+      client: "Celine Buch",
+      startDate: "2024-01-05",
+      duration: 24,
+      comments: "This is not boring lorem ipsum text.",
+    },
+    {
+      contractName: "Contract 4",
+      client: "Severus Snape",
+      startDate: "2024-01-05",
+      duration: 24,
+      comments: "Obviously.",
+    },
+    {
+      contractName: "Contract 5",
+      client: "Robin Hood",
+      startDate: "2023-07-11",
+      duration: 24,
+      comments: "I live in Sherwood Forest.",
+    },
+  ];
+
+  const clients = [
+    "John Doe",
+    "Anne Hathaway",
+    "Celine Buch",
+    "Severus Snape",
+    "Robin Hood",
+  ];
+
+  const handleFilterChange = (e) => {
+    // here comes filter logic
+    console.log("Filter changed:", e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        CMS - It's Contract Managment System, not Content Managment System:D
+      </h1>
+      <ContractForm />
+      <ContractFilter
+        clients={clients}
+        handleFilterChange={handleFilterChange}
+      />
+      <ContractTable contracts={contracts} />
     </div>
   );
-}
+};
 
 export default App;
