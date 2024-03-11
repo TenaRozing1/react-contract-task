fbc0fe915dc63171ce10f0496120d2b1d91a9ada
Tena Rozing <tena.rozing@gauss.dev>
2024-03-11 09:27:58 +0100

    Added scss and classes for style component

--- src/components/contract-form/contract-form.component.jsx
+++ src/components/contract-form/contract-form.component.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./contract-form.styles.scss";
 
 const ContractForm = ({ clients, handleAddContract }) => {
   const [contractData, setContractData] = useState({
@@ -31,52 +32,77 @@ const ContractForm = ({ clients, handleAddContract }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="contractName"
-        value={contractData.contractName}
-        onChange={handleChange}
-        placeholder="Contract Name"
-        required
-      />
-      <select
-        name="client"
-        value={contractData.client}
-        onChange={handleChange}
-        required
-      >
-        <option value="">Select Client</option>
-        {clients.map((client, index) => (
-          <option key={index} value={client}>
-            {client}
-          </option>
-        ))}
-      </select>
-      <input
-        type="date"
-        name="startDate"
-        value={contractData.startDate}
-        onChange={handleChange}
-        placeholder="Start Date"
-        required
-      />
-      <input
-        type="number"
-        name="duration"
-        value={contractData.duration}
-        onChange={handleChange}
-        placeholder="Duration (months)"
-        required
-      />
-      <textarea
-        name="comments"
-        value={contractData.comments}
-        onChange={handleChange}
-        placeholder="Comments"
-      ></textarea>
-      <button type="submit">Add new contract</button>
-    </form>
+    <>
+      <h1 className="form-title">Fill the form to add contract</h1>
+      <form onSubmit={handleSubmit} className="contract-form">
+        <div className="form-row">
+          <label className="contract-form-label">
+            Contract Name:
+            <input
+              label="Contract Name"
+              type="text"
+              name="contractName"
+              value={contractData.contractName}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label className="contract-form-label">
+            Client:
+            <select
+              name="client"
+              value={contractData.client}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Select Client</option>
+              {clients.map((client, index) => (
+                <option key={index} value={client}>
+                  {client}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+        <div className="form-row">
+          <label className="contract-form-label">
+            Start Date:
+            <input
+              type="date"
+              name="startDate"
+              value={contractData.startDate}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label className="contract-form-label">
+            Duration (months):
+            <input
+              type="number"
+              name="duration"
+              value={contractData.duration}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div className="form-row">
+          <label className="contract-form-label">
+            Comments:
+            <textarea
+              name="comments"
+              value={contractData.comments}
+              onChange={handleChange}
+            ></textarea>
+          </label>
+        </div>
+        <div className="button-wrapper">
+          <button type="submit" className="submit-button">
+            Add new contract
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
